532ba5fb5fa8732af39d1922b5a3b80d3e722c81
weathergit <qtianaddress@gmail.com>
2022-09-21 16:14:02 +0800

    3x1-figure-1X3

--- src/cross_test_plot.py
+++ src/cross_test_plot.py
@@ -8,33 +8,44 @@ plt.rcParams['ytick.labelsize'] = 12
 
 def main(lab, mixed, field):
     colors = {'lab': '#AE3D3A', 'mixed': '#3382A3', 'field': '#304E6C'}
+    models_labels = ['EfficientNet B0', 'MobileNet V3s', 'ResNet34']
 
-    fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(5, 9), facecolor='w')
+    fig, axs = plt.subplots(nrows=1, ncols=3, sharex=True, figsize=(15, 5), facecolor='w')
     # lab on min field
-    axs[0].bar(x=np.arange(3) - 0.1, height=lab['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
-    axs[0].bar(x=np.arange(3) + 0.1, height=lab['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
+    axs[0].bar(x=np.arange(3) - 0.1, height=lab['acc'].values[0::2], width=0.2, color=colors['field'], label='Test on '
+                                                                                                             'Field')
+    axs[0].bar(x=np.arange(3) + 0.1, height=lab['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Test on '
+                                                                                                             'Mixed')
     axs[0].set_ylim(0, 1)
-    axs[0].set_ylabel('Lab', fontsize=16)
-    axs[0].legend(loc=9, ncol=2, frameon=False, fontsize=12)
+    axs[0].set_ylabel('Train on Lab', fontsize=20)
+    axs[0].legend(loc=9, ncol=2, frameon=False, fontsize=14)
+    axs[0].set_xticks(np.arange(3), labels=models_labels, fontsize=15)
 
     # Mixed on lab field
-    axs[1].bar(x=np.arange(3) - 0.1, height=mixed['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
-    axs[1].bar(x=np.arange(3) + 0.1, height=mixed['acc'].values[1::2], width=0.2, color=colors['lab'], label='Lab')
+    axs[1].bar(x=np.arange(3) - 0.1, height=mixed['acc'].values[0::2], width=0.2, color=colors['field'], label='Test '
+                                                                                                               'on '
+                                                                                                               'Field')
+    axs[1].bar(x=np.arange(3) + 0.1, height=mixed['acc'].values[1::2], width=0.2, color=colors['lab'], label='Test on '
+                                                                                                             'Lab')
     axs[1].set_ylim(0, 1)
-    axs[1].set_ylabel('Mixed', fontsize=16)
-    axs[1].legend(loc=9, ncol=2, frameon=False, fontsize=12)
+    axs[1].set_ylabel('Train on Mixed', fontsize=20)
+    axs[1].legend(loc=9, ncol=2, frameon=False, fontsize=14)
+    axs[1].set_xticks(np.arange(3), labels=models_labels, fontsize=15)
     # Field on mixed lab
-    axs[2].bar(x=np.arange(3) - 0.1, height=field['acc'].values[0::2], width=0.2, color=colors['lab'], label='Lab')
-    axs[2].bar(x=np.arange(3) + 0.1, height=field['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
+    axs[2].bar(x=np.arange(3) - 0.1, height=field['acc'].values[0::2], width=0.2, color=colors['lab'], label='Test on '
+                                                                                                             'Lab')
+    axs[2].bar(x=np.arange(3) + 0.1, height=field['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Test '
+                                                                                                               'on '
+                                                                                                               'Mixed')
     axs[2].set_ylim(0, 1)
-    axs[2].legend(loc=9, ncol=2, frameon=False, fontsize=12)
-    axs[2].set_ylabel('Field', fontsize=16)
-    models_labels = ['EfficientNet B0', 'MobileNet V3s', 'ResNet34']
-    plt.xticks(np.arange(3), labels=models_labels, fontsize=12)
-    plt.yticks(fontsize=12)
+    axs[2].legend(loc=9, ncol=2, frameon=False, fontsize=14)
+    axs[2].set_ylabel('Train on Field', fontsize=20)
+
+    axs[2].set_xticks(np.arange(3), labels=models_labels, fontsize=15)
+    plt.yticks(fontsize=14)
     # plt.xlim(-0.2, 3.4)
     plt.tight_layout()
-    plt.savefig('../fig/cross_test.png', dpi=300)
+    plt.savefig('../fig/cross_test_row.png', dpi=300)
     plt.show()
 
 
