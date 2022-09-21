2ddc5be39c7a8ffae56f7c8bb1dadb5fc4169087
weathergit <qtianaddress@gmail.com>
2022-09-21 14:46:20 +0800

    代码模块化

--- src/cross_test_plot.py
+++ src/cross_test_plot.py
@@ -5,40 +5,46 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams['ytick.labelsize'] = 12
 
-result = pd.read_csv('../data/total_cross_test_frame.csv')
 
-field = result[result['sources'] == 'field']
+def main(lab, mixed, field):
+    colors = {'lab': '#AE3D3A', 'mixed': '#3382A3', 'field': '#304E6C'}
 
-lab = result[result['sources'] == 'lab']
+    fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(5, 9), facecolor='w')
+    # lab on min field
+    axs[0].bar(x=np.arange(3) - 0.1, height=lab['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
+    axs[0].bar(x=np.arange(3) + 0.1, height=lab['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
+    axs[0].set_ylim(0, 1)
+    axs[0].set_ylabel('Lab', fontsize=16)
+    axs[0].legend(loc=9, ncol=2, frameon=False, fontsize=12)
 
-mixed = result[result['sources'] == 'mixed']
+    # Mixed on lab field
+    axs[1].bar(x=np.arange(3) - 0.1, height=mixed['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
+    axs[1].bar(x=np.arange(3) + 0.1, height=mixed['acc'].values[1::2], width=0.2, color=colors['lab'], label='Lab')
+    axs[1].set_ylim(0, 1)
+    axs[1].set_ylabel('Mixed', fontsize=16)
+    axs[1].legend(loc=9, ncol=2, frameon=False, fontsize=12)
+    # Field on mixed lab
+    axs[2].bar(x=np.arange(3) - 0.1, height=field['acc'].values[0::2], width=0.2, color=colors['lab'], label='Lab')
+    axs[2].bar(x=np.arange(3) + 0.1, height=field['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
+    axs[2].set_ylim(0, 1)
+    axs[2].legend(loc=9, ncol=2, frameon=False, fontsize=12)
+    axs[2].set_ylabel('Field', fontsize=16)
+    models_labels = ['EfficientNet B0', 'MobileNet V3s', 'ResNet34']
+    plt.xticks(np.arange(3), labels=models_labels, fontsize=12)
+    plt.yticks(fontsize=12)
+    # plt.xlim(-0.2, 3.4)
+    plt.tight_layout()
+    plt.savefig('../fig/cross_test.png', dpi=300)
+    plt.show()
 
-colors = {'lab': '#AE3D3A', 'mixed': '#3382A3', 'field': '#304E6C'}
 
-fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(5, 9), facecolor='w')
-# lab on min field
-axs[0].bar(x=np.arange(3)-0.1, height=lab['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
-axs[0].bar(x=np.arange(3)+0.1 , height=lab['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
-axs[0].set_ylim(0, 1)
-axs[0].set_ylabel('Lab', fontsize=16)
-axs[0].legend(loc=9, ncol=2, frameon=False, fontsize=12)
+if __name__ == "__main__":
+    result = pd.read_csv('../data/total_cross_test_frame.csv')
 
-# Mixed on lab field
-axs[1].bar(x=np.arange(3)-0.1, height=mixed['acc'].values[0::2], width=0.2, color=colors['field'], label='Field')
-axs[1].bar(x=np.arange(3) + 0.1, height=mixed['acc'].values[1::2], width=0.2, color=colors['lab'], label='Lab')
-axs[1].set_ylim(0, 1)
-axs[1].set_ylabel('Mixed', fontsize=16)
-axs[1].legend(loc=9, ncol=2, frameon=False, fontsize=12)
-# Field on mixed lab
-axs[2].bar(x=np.arange(3)-0.1, height=field['acc'].values[0::2], width=0.2, color=colors['lab'], label='Lab')
-axs[2].bar(x=np.arange(3) + 0.1, height=field['acc'].values[1::2], width=0.2, color=colors['mixed'], label='Mixed')
-axs[2].set_ylim(0, 1)
-axs[2].legend(loc=9, ncol=2, frameon=False, fontsize=12)
-axs[2].set_ylabel('Field', fontsize=16)
-models_labels = ['EfficientNet B0', 'MobileNet V3s', 'ResNet34']
-plt.xticks(np.arange(3), labels=models_labels, fontsize=12)
-plt.yticks(fontsize=12)
-# plt.xlim(-0.2, 3.4)
-plt.tight_layout()
-plt.savefig('../fig/cross_test.png', dpi=300)
-plt.show()
+    field_df = result[result['sources'] == 'field']
+
+    lab_df = result[result['sources'] == 'lab']
+
+    mixed_df = result[result['sources'] == 'mixed']
+
+    main(lab_df, mixed_df, field_df)
